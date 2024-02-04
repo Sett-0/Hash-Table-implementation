44ff8aa928e81017ea3758d6b6bcd3b3f88aeffb
Sett-0 <ampilov2000@mail.ru>
2024-02-05 00:11:01 +0300

    collision handling

--- hash_table.py
+++ hash_table.py
@@ -1,12 +1,15 @@
 class HashTable():
-    def __init__(self, List=None, size=10, grow_factor=2, size_threshold=0.75):
+    def __init__(self, List=None, size=13, grow_factor=2, size_threshold=0.75):
         self.grow_factor = grow_factor
         self.size_threshold = size_threshold
         if List is None:
+            self.table = [self.LinkedList() for _ in range(size)]
             self.size = size
-            self.table = [None] * size
+            self.number_of_elements = 0
         else:
-            self.list_to_hash_table()
+            self.table = self.list_to_hash_table(List)
+            self.size = len(self.table)
+            self.number_of_elements = len(List)
     
     def hash(self, key):
         if isinstance(key, int):
@@ -16,48 +19,120 @@ class HashTable():
             for letter in key:
                 hash_value += ord(letter)
                 hash_value *= ord(letter)
-            return hash_value % self.size
+            return hash_value
         else:
             raise Exception('Invalid key!')
 
-    def add(self, key, data=None):
-        if self.size * self.size_threshold > len(self.table) + 1:
-            self.grow_table()
-        index = self.hash(key)
-        self.table[index] = (key, data)
+    def index(self, key):
+        return self.hash(key) % self.size
+    
+    class LinkedList:
+        def __init__(self):
+            self.head = None
         
-    def remove(self, key):
-        index = self.hash(key)
-        self.table[index] = None
+        def __iter__(self):
+            node = self.head 
+            while node is not None:
+                yield node
+                node = node.next
         
-    def change_data(self, key, data):
-        index = self.hash(key)
-        self.table[index] = (key, data)
+        def add_first(self, node):
+            node.next = self.head 
+            self.head = node 
         
-    def print(self):
-        print('------\t START \t------')
-        for el in self.table:
-            if el is None:
-                print(el)
-            else:
-                print(el[0], ': ', el[1], sep='')
-        print('------\t END \t------')
+        def remove_node(self, key):
+            if self.head is None:
+                raise Exception('Element not found!')
+            if self.head.data[0] == key:
+                self.head = self.head.next
+                return
+            prev_node = self.head 
+            for node in self:
+                if node.data[0] == key:
+                    prev_node.next = node.next 
+                    return 
+                prev_node = node 
+            raise Exception('Element not found!')
+        
+        def contains_key(self, key):
+            for current_node in self:
+                if current_node.data[0] == key:
+                    return True 
+            return False
+        
+        class Node:
+            def __init__(self, data):
+                self.data = data 
+                self.next = None 
     
     def list_to_hash_table(self, List):
-        pass
+        size = int(len(List) * self.grow_factor)
+        table = [self.LinkedList() for _ in range(size)]
+        for el in List:
+            if not isinstance(el, tuple):
+                el = (el, None)
+            index = self.hash(el[0]) % len(table)
+            node = self.LinkedList.Node(data=el)
+            table[index].add_first(node)
+        return table
     
+    def add(self, key, data=None):
+        index = self.index(key)
+        if self.table[index].contains_key(key): 
+            return 
+        self.table[index].add_first(self.LinkedList.Node(data=(key, data)))
+        
+        if self.size * self.size_threshold > self.number_of_elements + 1:
+            self.grow_table()
+        
+    def remove(self, key):
+        index = self.index(key)
+        self.table[index].remove_node(key)
+        
+    def change_data(self, key, data):
+        index = self.index(key)
+        for node in self.table[index]:
+            if node is None:
+                raise Exception('Element not found!')
+            if node.data[0] == key:
+                node.data = (key, data)
+        
     def grow_table(self):
         pass
+
+    def __repr__(self):
+        nodes = ["------\t START \t------\n"]
+        for llist in self.table:
+            node = llist.head 
+            if node is None:
+                nodes.append("None\n")
+            else:
+                while node.next is not None:
+                    nodes.append(f"{node.data[0]}: {node.data[1]}, ")
+                    node = node.next
+                nodes.append(f"{node.data[0]}: {node.data[1]}\n")
+        nodes.append("------\t END \t------\n")
+        return ''.join(nodes)
+    
+    def contains(self, key):
+        index = self.index(key)
+        if self.table[index].contains_key(key): 
+            return True
+        return False 
     
 if __name__ == '__main__':
-    people = HashTable()
-    names = ['oleg', 'igor', 'sasha']
+    people = HashTable(size=2)
+    names = ['Oleg', 'Igor', 'Sasha']
     ages = [21, 53, 17]
     for name, age in zip(names, ages):
         people.add(name, age)
     
-    people.print()
+    print(people)
     people.change_data(names[1], 54)
-    people.print()
-    people.remove('sasha')
-    people.print()
+    print(people)
+    people.remove('Oleg')
+    print(people)
+    
+    people_list = list(zip(names, ages))
+    people = HashTable(people_list)
+    print(people)
